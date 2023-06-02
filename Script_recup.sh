8884294b20553e1bdfac7183f5c27e1be74e8cce
Hugo0707 <133368633+Hugo0707@users.noreply.github.com>
2023-06-02 16:40:35 +0200

    Update Script_recup.sh
    Troisième script bash de récupération de données

--- Script_recup.sh
+++ Script_recup.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-# paramètre de connexion mqtt
-mqtt_nom_domaine="localhost"
-mqtt_port="1883"
-mqtt_topic="Student/by-room/E101/data"
-
-# récupération des données en mqtt
-data_E101=$(mosquitto_sub -h $mqtt_nom_domaine -p $mqtt_port -t $mqtt_topic -C 1)
-
-# traitement des données de la salle
-temp_E101=$(echo $data_E101 | jq '.[0].temperature')
-co2_E101=$(echo $data_E101 | jq '.[0].co2')
-
-date=$(date +"%Y-%m-%d %H:%M:%S")
-
-ID_sensor="2"
-
 # paramètre de connexion à la base de données
 db_user="admin"
 db_pass="sae23"
 db_host="localhost"
 db_base="sae23"
-db_table_name="measure"
+
+> data.txt
+
+result=$(/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -sN -e "SELECT ID_sensor,Type_sensor, Room FROM sensor" | jq -R . | jq -s .)
+
+for data in $(echo "$result" | jq -r '.[]')
+do
+	echo $data >> data.txt
+done
+
+nbr_ligne=$(wc -l < ./data.txt)
+nbr_sensor=$(($nbr_ligne / 3))
+
+
+for ((i=1; i<=$nbr_sensor; i++))
+do
+
+ID_sensor=$(sed '1q;d' ./data.txt)
+Type_sensor=$(sed '2q;d' ./data.txt)
+Room=$(sed '3q;d' ./data.txt)
+
+# paramètre de connexion mqtt
+mqtt_nom_domaine="localhost"
+mqtt_port="1883"
+mqtt_topic=$(echo -e "Student/by-room/$Room/data")
+
+# récupération des données en mqtt
+all_data=$(mosquitto_sub -h $mqtt_nom_domaine -p $mqtt_port -t "$mqtt_topic" -C 1)
+quote=\'
+# traitement des données de la salle
+data=$(echo "$all_data" | jq '.[0].'"$Type_sensor"'')
+
+date=$(date +"%Y-%m-%d %H:%M:%S")
+
+echo $data
 
 # Requête SQL d'insertion de données
-insert_temp="INSERT INTO $db_table_name (ID_Sensor, Date, Value) VALUES ('$ID_sensor','$date', '$temp_E101');"
-insert_co2="INSERT INTO $db_table_name (ID_Sensor, Date, Value) VALUES ('$ID_sensor','$date', '$co2_E101');"
+insert_data="INSERT INTO measure (ID_Sensor, Date, Value) VALUES ('$ID_sensor','$date', '$data');"
 
 # Exécution de la requête SQL
-/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_temp"
-/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_co2"
+/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_data"
+
+sed -i '1,3d' data.txt
+
+done
 
