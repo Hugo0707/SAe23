534177fa7516fc7717abb04e2912a43aa4d80025
Hugo0707 <133368633+Hugo0707@users.noreply.github.com>
2023-06-02 16:39:44 +0200

    Update Script_recup.sh
    Second script bash de récupération de données

--- Script_recup.sh
+++ Script_recup.sh
@@ -14,5 +14,20 @@ co2_E101=$(echo $data_E101 | jq '.[0].co2')
 
 date=$(date +"%Y-%m-%d %H:%M:%S")
 
-echo "temp : "$temp_E101 ",co2 :" $co2_E101 ",date : "$date >> ./data.txt
+ID_sensor="2"
+
+# paramètre de connexion à la base de données
+db_user="admin"
+db_pass="sae23"
+db_host="localhost"
+db_base="sae23"
+db_table_name="measure"
+
+# Requête SQL d'insertion de données
+insert_temp="INSERT INTO $db_table_name (ID_Sensor, Date, Value) VALUES ('$ID_sensor','$date', '$temp_E101');"
+insert_co2="INSERT INTO $db_table_name (ID_Sensor, Date, Value) VALUES ('$ID_sensor','$date', '$co2_E101');"
+
+# Exécution de la requête SQL
+/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_temp"
+/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_co2"
 
