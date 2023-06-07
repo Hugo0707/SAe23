d50fb48c328298178aec24c006e794f32df484a2
Hugo0707 <133368633+Hugo0707@users.noreply.github.com>
2023-06-07 21:49:40 +0200

    Update Script_recup.sh
    Finalisation, indentation et changement de nom de quelques variables. Version final du projet.

--- Script_recup.sh
+++ Script_recup.sh
@@ -19,31 +19,32 @@ do
 done
 
 # calculate the number of sensors to process
-nbr_ligne=$(wc -l < ./data.txt)
-nbr_sensor=$(($nbr_ligne / 3))
+nbr_line=$(wc -l < ./data.txt)
+nbr_sensor=$(($nbr_line / 3))
 
 # creation of the recovery function and sends data
-fonction(){
+fonction()
+{
 
-# mqtt connection parameter
-mqtt_nom_domaine="localhost"
-mqtt_port="1883"
-mqtt_topic=$(echo "Student/by-room/$3/data")
+	# mqtt connection parameter
+	mqtt_domain_name="mqtt.iut-blagnac.fr"
+	mqtt_port="1883"
+	mqtt_topic=$(echo "Student/by-room/$3/data")
 
-# mqtt data recovery
-all_data=$(mosquitto_sub -h $mqtt_nom_domaine -p $mqtt_port -t "$mqtt_topic" -C 1)
+	# mqtt data recovery
+	all_data=$(mosquitto_sub -h $mqtt_domain_name -p $mqtt_port -t "$mqtt_topic" -C 1)
 
-# room data processing
-data=$(echo "$all_data" | jq '.[0].'$2)
+	# room data processing
+	data=$(echo "$all_data" | jq '.[0].'$2)
 
-# date and time retrieval
-date=$(date +"%Y-%m-%d %H:%M:%S")
+	# date and time retrieval
+	date=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Data Insertion SQL Query
-insert_data="INSERT INTO measure (ID_Sensor, Date, Value) VALUES ('$1','$date', '$data');"
+	# Data Insertion SQL Query
+	insert_data="INSERT INTO measure (ID_Sensor, Date, Value) VALUES ('$1','$date', '$data');"
 
-# SQL query execution
-/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_data"
+	# SQL query execution
+	/opt/lampp/bin/mysql -h $db_host -D $db_base -u $db_user -p$db_pass -e "$insert_data"
 
 }
 
@@ -51,20 +52,20 @@ insert_data="INSERT INTO measure (ID_Sensor, Date, Value) VALUES ('$1','$date', 
 for ((i=1; i<=$nbr_sensor; i++))
 do
 
-# Sensor ID retrieval
-ID_sensor=$(sed '1q;d' ./data.txt)
+	# Sensor ID retrieval
+	ID_sensor=$(sed '1q;d' ./data.txt)
 
-# Sensor type recovery
-Type_sensor=$(sed '2q;d' ./data.txt)
+	# Sensor type recovery
+	Type_sensor=$(sed '2q;d' ./data.txt)
 
-# Sensor Room Recovery
-Room=$(sed '3q;d' ./data.txt)
+	# Sensor Room Recovery
+	Room=$(sed '3q;d' ./data.txt)
 
-# Parallel execution of the function
-fonction "$ID_sensor" "$Type_sensor" "$Room" &
+	# Parallel execution of the function
+	fonction "$ID_sensor" "$Type_sensor" "$Room" &
 
-# Deletion of the first 3 lines of the file containing the information of the sensors
-sed -i '1,3d' data.txt
+	# Deletion of the first 3 lines of the file containing the information of the sensors
+	sed -i '1,3d' data.txt
 
 # End
 done
